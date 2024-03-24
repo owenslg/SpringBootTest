54eeeef41b045aa15c2d08fb00930dc43f1ce88f
owenslg <owenslg@g.cofc.edu>
2024-03-24 13:01:04 -0400

    minor testing

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -20,13 +20,14 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping
+    @GetMapping//this annotation tells the app what to do when it gets a get request
     public List<Student> getStudents()
     {
         return studentService.getStudents();
     }
 
-    @PostMapping
+
+    @PostMapping//this annotation tells the app what to do when it gets a post request
     public void registerNewStudent(@RequestBody Student student)//this annotation maps the request body contents onto a student object
     {
         studentService.addNewStudent(student);
@@ -35,18 +36,17 @@ public class StudentController {
 
     //the "pathvariable is referring to the content that comes after the final '/' in the URI path
     //In this case if the DELETE request is sent to http://localhost:8080/api/v1/student/1 then it will pass '1' as the pathvariable
-    @DeleteMapping(path = "{studentId}")
+    @DeleteMapping(path = "{studentId}") //this annotation tells the app what to do when it gets a delete request
     public void deleteStudent(@PathVariable("studentId") Long studentId)
     {
         studentService.deleteStudent(studentId);
     }
 
-    @PutMapping(path  = "{studentId}")
+    @PutMapping(path  = "{studentId}") //this annotation tells the app what to do when it gets a put request
     public void updateStudent(@PathVariable("studentId") Long studentId,
                               @RequestParam(required = false) String name,
                               @RequestParam(required = false) String email)
     {
         studentService.updateStudent(studentId, name, email);
     }
-
 }
